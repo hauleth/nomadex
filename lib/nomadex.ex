ba695454832639a67161f7280cda6fca4fe149be
Łukasz Jan Niemier <lukasz@niemier.pl>
2017-04-16 01:13:25 +0200

    Initial commit

---
+++ lib/nomadex.ex
@@ -0,0 +1,18 @@
+defmodule Nomadex do
+  @moduledoc """
+  Documentation for Nomadex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Nomadex.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nomadex_test.exs
@@ -0,0 +1,8 @@
+defmodule NomadexTest do
+  use ExUnit.Case
+  doctest Nomadex
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
